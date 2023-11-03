3409b75550ddd3f8f4797101ba94873f59dbfd0d
Lluvia0802 <xzho4299@uni.sydney.edu.au>
2023-11-03 13:03:53 +0800

    Keep an element in the center of the window

--- sketch.js
+++ sketch.js
@@ -1,20 +1,5 @@
 let firework1;
-let firework2;
-let firework3;
-let firework4;
-let firework5;
-let firework6;
-let firework7;
-let firework8;
-let firework9;
-let firework10;
-let firework11;
-let firework12;
-let firework13;
-let firework14;
-let firework15;
-let firework16;
-let firework17;
+
 // Control the number of frames in the bloom cycle
 let cycleDuration = 2000;
 let whiteDots = [];
@@ -25,23 +10,7 @@ function setup() {
   angleMode(DEGREES);
 
   // Create 17 fireworks
-  firework1 = new Firework(0.14 * width, 0.13 * height, 0.5, 1);
-  firework2 = new Firework(0.43 * width, 0.06 * height, 0.5, 1);
-  firework3 = new Firework(0.73 * width, -0.01 * height, 0.5, 1);
-  firework4 = new Firework(0.04 * width, 0.4 * height, 0.5, 1);
-  firework5 = new Firework(0.33 * width, 0.36 * height, 0.5, 0);
-  firework6 = new Firework(0.63 * width, 0.27 * height, 0.5, 0);
-  firework7 = new Firework(0.95 * width, 0.21 * height, 0.5, 1);
-  firework8 = new Firework(-0.02 * width, 0.7 * height, 0.5, 1);
-  firework9 = new Firework(0.24 * width, 0.63 * height, 0.5, 0);
-  firework10 = new Firework(0.56 * width, 0.56 * height, 0.5, 0);
-  firework11 = new Firework(0.86 * width, 0.49 * height, 0.5, 1);
-  firework12 = new Firework(0.16 * width, 0.91 * height, 0.5, 1);
-  firework13 = new Firework(0.47 * width, 0.85 * height, 0.5, 1);
-  firework14 = new Firework(0.76 * width, 0.77 * height, 0.5, 1);
-  firework15 = new Firework(width, 0.74 * height, 0.5, 1);
-  firework16 = new Firework(0.67 * width, 1.05 * height, 0.5, 1);
-  firework17 = new Firework(0.95 * width, height, 0.5, 1);
+  firework1 = new Firework(0.5 * width, 0.5 * height, 0.5, 1);
 
   // Create randomly distributed white dots
   for (let i = 0; i < 50; i++) {
@@ -63,61 +32,12 @@ function draw() {
   }
 
   firework1.show();
-  firework1.update();
-  firework2.show();
-  firework2.update();
-  firework3.show();
-  firework3.update();
-  firework4.show();
-  firework4.update();
-  firework5.show();
-  firework5.update();
-  firework6.show();
-  firework6.update();
-  firework7.show();
-  firework7.update();
-  firework8.show();
-  firework8.update();
-  firework9.show();
-  firework9.update();
-  firework10.show();
-  firework10.update();
-  firework11.show();
-  firework11.update();
-  firework12.show();
-  firework12.update();
-  firework13.show();
-  firework13.update();
-  firework14.show();
-  firework14.update();
-  firework15.show();
-  firework15.update();
-  firework16.show();
-  firework16.update();
-  firework17.show();
-  firework17.update();
 
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  firework1.updatePosition(0.14 * width, 0.13 * height);
-  firework2.updatePosition(0.43 * width, 0.06 * height);
-  firework3.updatePosition(0.73 * width, -0.01 * height);
-  firework4.updatePosition(0.04 * width, 0.4 * height);
-  firework5.updatePosition(0.33 * width, 0.36 * height);
-  firework6.updatePosition(0.63 * width, 0.27 * height);
-  firework7.updatePosition(0.95 * width, 0.21 * height);
-  firework8.updatePosition(-0.02 * width, 0.7 * height);
-  firework9.updatePosition(0.24 * width, 0.63 * height);
-  firework10.updatePosition(0.56 * width, 0.56 * height);
-  firework11.updatePosition(0.86 * width, 0.49 * height);
-  firework12.updatePosition(0.16 * width, 0.91 * height);
-  firework13.updatePosition(0.47 * width, 0.85 * height);
-  firework14.updatePosition(0.76 * width, 0.77 * height);
-  firework15.updatePosition(width, 0.74 * height);
-  firework16.updatePosition(0.67 * width, 1.05 * height);
-  firework17.updatePosition(0.95 * width, height);
+  firework1.updatePosition(0.5 * width, 0.5 * height);
 
   // Update the position of the white dot
   for (let dot of whiteDots) {
